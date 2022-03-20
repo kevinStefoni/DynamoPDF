0d0b9cdecb3472b2486b1c33192aa78f1dd1388b
Kevin <kevinStefoni@gmail.com>
2022-03-19 21:17:38 -0500

    Small update to FXML files and UserInputController.
    Linked FXML files to the application.css file, rather than leaving the
    style inside the FXML file.

--- DynamoPDF/src/application/controller/UserInputController.java
+++ DynamoPDF/src/application/controller/UserInputController.java
@@ -1,5 +1,11 @@
 package application.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class UserInputController extends OptionsMenuController{
 
 
@@ -10,6 +16,29 @@ public class UserInputController extends OptionsMenuController{
 		 * worksheet.getOptions().getHasDate()
 		 */
 		
-	
+
+    @FXML
+    private TextField question1a;
+
+    @FXML
+    private TextField question2a;
+    
+    @FXML
+    private TextField question3a;
+    
+    @FXML
+    private TextField question4a;
+    
+    @FXML
+    private TextField question5a;
+
+    @FXML
+    private AnchorPane background;
+
+    @FXML
+    private TextArea questionBox;
+
+    @FXML
+    private Button questionInputNextButton;
 	
 }
