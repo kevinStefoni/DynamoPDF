acbb1518104e24ba8d0ca0b0d3bb212fcca56f2d
Kevin <kevinStefoni@gmail.com>
2022-03-21 18:29:57 -0500

    PDFSceneController deleted

--- DynamoPDF/src/application/controller/PDFController.java
+++ DynamoPDF/src/application/controller/PDFController.java
@@ -1,15 +1,42 @@
 package application.controller;
 
+import java.io.IOException;
+
 import application.model.PDF;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
 
 public class PDFController extends UserInputController{
 
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private MenuBar menuBar;
+    
+    @FXML
+    private TextArea PDFNameTextArea;
+    
+    @FXML
+    private Menu editMenu;
+
+    @FXML
+    private Menu helpMenu;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private Menu fileMenu;
+	
     @FXML
     private AnchorPane generatePDFAnchorPane;
 
@@ -34,5 +61,11 @@ public class PDFController extends UserInputController{
 		pdf.generatePDF(worksheet);
 		
 	}
+	
+    @FXML
+    void handleSave(ActionEvent event) throws IOException {
+    	String pdfName=PDFNameTextArea.getText().toString()+"\n";
+    	
+    }
 	
 }
